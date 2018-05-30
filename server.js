720a3d11aeb2d72a3bbcd7cb0efecf465cf9f98e
iamharbie <mi_haby@live.com>
2018-05-30 17:46:58 +0100

    Added env var to recognize PORT on host machine

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app =  express();
+const port = process.env.PORT || 3000; 
 
 
 hbs.registerPartials(path.join(__dirname,'views','partials'));
@@ -85,6 +86,6 @@ app.get('/about',(req,res)=>{
 
 
 
-app.listen(3000,()=>{
-    console.log('Server is up and running on port 3000');
+app.listen(port,()=>{
+    console.log(`Server is up and running on port ${port}`);
 });
